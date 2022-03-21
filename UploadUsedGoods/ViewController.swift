086ac7b9914592abf589648927d72d04267c9e05
min-kyoung <shalsrud9@naver.com>
2022-03-21 16:54:17 +0900

    Initial Commit

---
+++ UploadUsedGoods/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UploadUsedGoods
+//
+//  Created by 노민경 on 2022/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
