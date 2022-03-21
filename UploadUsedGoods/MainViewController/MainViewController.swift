2d129de082ded344fa5df4364fdd6110f8709737
min-kyoung <shalsrud9@naver.com>
2022-03-21 17:26:23 +0900

    Add : MainViewController

---
+++ UploadUsedGoods/MainViewController/MainViewController.swift
@@ -0,0 +1,67 @@
+//
+//  MainViewController.swift
+//  UploadUsedGoods
+//
+//  Created by 노민경 on 2022/03/21.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+
+class MainViewController: UIViewController {
+    let disposeBag = DisposeBag()
+    
+    let tableView = UITableView()
+    let submitButton = UIBarButtonItem()
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        attribute()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func bind(_ viewModel: MainViewModel) {
+        
+    }
+    
+    private func attribute() {
+        title = "중고거래 글쓰기"
+        view.backgroundColor = .white
+        
+        submitButton.title = "제출"
+        submitButton.style = .done
+        
+        navigationItem.setRightBarButton(submitButton, animated: true)
+        
+        tableView.backgroundColor = .white
+        tableView.separatorStyle = .singleLine
+        tableView.tableFooterView = UIView()
+    }
+    
+    private func layout() {
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+typealias Alert = (title: String, message: String?)
+extension Reactive where Base: MainViewController {
+    var setAlert: Binder<Alert> { // Alert이라는 binder를 전달받음
+        return Binder(base) { base, data in
+            let alertController = UIAlertController(title: data.title, message: data.message, preferredStyle: .alert)
+            let action = UIAlertAction(title: "확인", style: .cancel, handler: nil)
+            alertController.addAction(action)
+            base.present(alertController, animated: true, completion: nil)
+        }
+    }
+}
---
+++ UploadUsedGoods/MainViewController/MainViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewModel.swift
+//  UploadUsedGoods
+//
+//  Created by 노민경 on 2022/03/21.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+struct MainViewModel {
+    
+}
